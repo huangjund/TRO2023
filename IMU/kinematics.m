648df401db7ccfcc71ada32a09cf90c10036801d
huangjund <ustcdraja@gmail.com>
2023-11-05 21:47:53 +0800

    small revisions

--- IMU/kinematics.m
+++ IMU/kinematics.m
@@ -1,7 +1,7 @@
 clear all;
 clc;
 
-syms q1 q2 q3 q4 q5 q6 q7
+syms q1 q2 q3 q4 q5 q6 q7 real
 
 H23 = [[cos(-pi/6) 0 -sin(-pi/6); 0 1 0; sin(-pi/6) 0 cos(-pi/6)] [-59.07/2+q1; 0; 60.16];...
     0 0 0 1];
@@ -23,23 +23,13 @@ H89 = [[cos(pi/2) -sin(pi/2) 0; sin(pi/2) cos(pi/2) 0; 0 0 1]*...
 H910 = [[1 0 0; 0 cos(q7) -sin(q7); 0 sin(q7) cos(q7)] [0;0;52.06];...
     0 0 0 1];
 
-H25 = simplify(H23*H34*H45);
-H29 = simplify(H27*H78*H89);
+% H25 = simplify(H23*H34*H45);
+% H29 = simplify(H27*H78*H89);
+H95 = TransInv(H89)*TransInv(H78)*TransInv(H27)*H23*H34*H45;
+Z = atan2(H95(3,2),H95(3,3)); Y = asin(-H95(3,1)); X = atan2(H95(2,1),H95(1,1));
+JZYX = simplify(jacobian([X;Y;Z],[q2,q3,q5,q6]));
 
-se3mat1 = MatrixLog6(simplify(TransInv(H29)*H25)); %% SE3 -> se3 R4x4
-V1 = se3ToVec(se3mat1); %% R6
+% se3mat1 = MatrixLog6(simplify(TransInv(H29)*H25)); %% SE3 -> se3 R4x4
+% V1 = se3ToVec(se3mat1); %% R6
 
-%[theta1,w_hat1]=R2so3(H29(1:3,1:3)'*H25(1:3,1:3));
-
-
-function [theta, w_hat] = R2so3(R)
-    if trace(R) == -1
-        theta = pi;
-        w_hat = 1/sqrt(2*(1+R(3,3)))*[R(1,3);R(2,3);R(3,3)+1]; %% unit vector
-        so3
-    else
-        theta = acos((trace(R)-1)/2);
-        W_hat = (R-R')/(2*sin(theta));
-        w_hat = [W_hat(3,2);W_hat(1,3);W_hat(2,1)];
-    end
-end
+[S45, theta45] = AxisAng6(se3ToVec(MatrixLog6(H45)));
